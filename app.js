00234c4fe8418ee27cdb63eb98f74d1ef8da1236
Oleksandr Sukhinin <syhinin@gmail.com>
2025-11-07 20:17:33 +0100

    add error, cookieParser, urlencoded  middlewares

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import userRoutes from "./routes/user.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import subscriptionRoutes from "./routes/subscriptions.router.js";
 import connectDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+// Middleware setup
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(errorMiddleware);
 
 // Mounting route handlers
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/subscriptions", subscriptionRoutes);
-
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
 
 app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ middlewares/error.middleware.js
@@ -0,0 +1,38 @@
+const errorMiddleware = (err, req, res, next) => {
+  try {
+    let error = { ...err };
+
+    error.message = err.message;
+
+    console.error(err);
+
+    //Mongoose bad ObjectId
+    if (err.name === "CastError") {
+      error = new Error("Resource not found");
+      error.statusCode = 404;
+    }
+
+    //Mongoose duplicate key
+    if (err.code === 11000) {
+      error = new Error("Duplicate field value entered");
+      error.statusCode = 400;
+    }
+    //Mongoose validation error
+    if (err.name === "ValidationError") {
+      error = new Error(
+        Object.values(err.errors)
+          .map((val) => val.message)
+          .join(", ")
+      );
+      error.statusCode = 400;
+    }
+
+    return res
+      .status(error.statusCode || 500)
+      .json({ success: false, error: error.message || "Server Error" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default errorMiddleware;
