2cd7f9957f1133ea98a527c5a0df8c83b3186380
Oleksandr Sukhinin <syhinin@gmail.com>
2025-11-07 15:42:22 +0100

    define base set-up with eslint

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!!!!");
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
