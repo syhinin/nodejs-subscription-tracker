76b253afc3513d114007000bfd8590da67ed2795
Oleksandr Sukhinin <syhinin@gmail.com>
2025-11-09 22:56:07 +0100

    add authMiddleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import User from "../models/mongoose/user.model.js";
+
+const authMiddleware = async (req, res, next) => {
+  // Authentication logic here
+  try {
+    let token;
+
+    if (req?.headers?.authorization?.startsWith("Bearer ")) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+    
+    if (!token) {
+      return res
+        .status(401)
+        .json({ success: false, message: "No token provided" });
+    }
+
+    // Verify token and extract user info
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return res.status(401).json({ success: false, message: "Invalid token" });
+    }
+
+    req.user = user; // Attach user to request object
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Token verification failed", error });
+  }
+};
+export default authMiddleware;
